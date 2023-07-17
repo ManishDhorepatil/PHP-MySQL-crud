9d9155292dd033ed979fb78dcbdfe5751c3bb5b9
Manish Dhorepatil <88958758+ManishDhorepatil@users.noreply.github.com>
2023-07-17 21:06:59 +0530

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,7 +1,7 @@
 <?php
 /// connection to database in php 
 $con=mysqli_connect('localhost','root','','crudseries');
-///condition to check if connection sucessfull or not
+///condition to check if connection sucessf ull or not
 if(!$con){
     die(mysqli_error($con));
 }
